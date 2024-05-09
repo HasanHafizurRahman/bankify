3a69326b4f3ddf97e398956d896707b041a786f2
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-05-09 16:58:11 +0600

    react countup chartjs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
@@ -12,6 +13,11 @@ const Home = () => {
             title="Welcome"
             user={loggedIn?.firstName || 'Guest'}
             subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
           />
         </header>
       </div>
