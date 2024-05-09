75cf159c4c0d35d739530217f8d40311ad0ce3c9
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-05-09 15:54:15 +0600

    header box

---
+++ app/(root)/page.tsx
@@ -0,0 +1,22 @@
+import HeaderBox from "@/components/HeaderBox";
+import React from "react";
+
+const Home = () => {
+  const loggedIn = {firstName: "Hasan"}
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+        <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
